c83178b7fff2881fad719b2229e4131b3048c0fc
Juan Manuel Castillo <jmkasti11@gmail.com>
2022-06-28 13:35:52 +0200

    cambios js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let simonDice = prompt ("Ingrese una palabra")
+
+while (simonDice != "ESC") {
+  alert ("Simon dice"+ " "+ simonDice);
+  simonDice = prompt ('Agregue otra palabra o escriba "ESC" para finalizar')
+  }
